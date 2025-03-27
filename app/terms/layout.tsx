2ee8de4f9b97ba9c8b613c897257bea23575716c
Nooc <nooc@nooc.me>
2025-03-27 22:22:50 +0800

    Fix the styles in dark mode

--- app/terms/layout.tsx
+++ app/terms/layout.tsx
@@ -3,7 +3,7 @@ export const runtime = "edge";
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
   return (
-    <div className="flex flex-col p-4 mt-10 mb-20 mx-auto prose">
+    <div className="flex flex-col p-4 mt-10 mb-20 mx-auto prose dark:prose-invert">
       {children}
     </div>
   );
