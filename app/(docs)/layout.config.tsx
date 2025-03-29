62be1fd9d5d149fdf5a2e55eb6704558df9f31e9
Nooc <nooc@nooc.me>
2025-03-29 21:49:02 +0800

    Update docs

--- app/(docs)/layout.config.tsx
+++ app/(docs)/layout.config.tsx
@@ -1,6 +1,5 @@
 import { Logo } from '@/components/Logo';
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import Image from 'next/image';
 
 /**
  * Shared layout configurations
@@ -14,13 +13,12 @@ export const baseOptions: BaseLayoutProps = {
     title: (
       <>
         <Logo className='h-6 text-orange-500' />
-        <span className="font-medium [.uwu_&]:hidden [header_&]:text-[15px]">
+        <span className="font-medium">
           Echobell
         </span>
       </>
     ),
     transparentMode: 'top',
-    enabled: false,
   },
   githubUrl: 'https://github.com/weelone/echobell.one',
 };
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,10 +3,10 @@ import { SVGProps } from "react";
 export const Logo = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg {...props} viewBox="0 0 230 203" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M229.48 5.18968C223.488 63.2832 174.393 108.591 114.716 108.591C55.1928 108.591 6.19691 63.5159 0 5.63764C37.2122 1.96514 76.4782 0 117.059 0C155.958 0 193.648 1.80557 229.48 5.18968Z" fill="currentColor" fill-opacity="0.9" />
-      <rect x="105.472" y="146.057" width="18.9653" height="56.8958" rx="9.48264" fill="currentColor" fill-opacity="0.9" />
-      <rect x="178.837" y="132.768" width="18.9653" height="56.8958" rx="9.48264" transform="rotate(-33.2639 178.837 132.768)" fill="currentColor" fill-opacity="0.9" />
-      <rect width="18.9653" height="56.8958" rx="9.48264" transform="matrix(-0.836153 -0.548497 -0.548497 0.836153 50.1242 132.768)" fill="currentColor" fill-opacity="0.9" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M229.48 5.18968C223.488 63.2832 174.393 108.591 114.716 108.591C55.1928 108.591 6.19691 63.5159 0 5.63764C37.2122 1.96514 76.4782 0 117.059 0C155.958 0 193.648 1.80557 229.48 5.18968Z" fill="currentColor" fillOpacity="0.9" />
+      <rect x="105.472" y="146.057" width="18.9653" height="56.8958" rx="9.48264" fill="currentColor" fillOpacity="0.9" />
+      <rect x="178.837" y="132.768" width="18.9653" height="56.8958" rx="9.48264" transform="rotate(-33.2639 178.837 132.768)" fill="currentColor" fillOpacity="0.9" />
+      <rect width="18.9653" height="56.8958" rx="9.48264" transform="matrix(-0.836153 -0.548497 -0.548497 0.836153 50.1242 132.768)" fill="currentColor" fillOpacity="0.9" />
     </svg>
   );
 };
