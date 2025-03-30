3fe03b6a3c52685ebc73eafec1d40d7a826266f9
Nooc <nooc@nooc.me>
2025-03-30 14:08:32 +0800

    Add new doc page "What is Echobell"

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,9 @@
 import type { MDXComponents } from 'mdx/types'
+import { Callout } from 'fumadocs-ui/components/callout';
  
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    Callout,
   }
 }
