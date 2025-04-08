0b80b79b648907cb3a659e6f36987697cfbd19f8
Nooc <nooc@nooc.me>
2025-04-08 17:44:16 +0800

    Update sitemap

--- app/[lang]/sitemap.ts
+++ app/[lang]/sitemap.ts
@@ -1,9 +1,9 @@
-import type { MetadataRoute } from 'next';
-import { baseUrl } from '@/lib/metadata';
-import { source, blog } from '@/lib/source';
-import { languages } from '@/lib/i18n';
+import type { MetadataRoute } from "next";
+import { baseUrl } from "@/lib/metadata";
+import { source, blog } from "@/lib/source";
+import { languages } from "@/lib/i18n";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 export const revalidate = false;
 
@@ -12,38 +12,53 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [
     {
-      url: url('/'),
-      changeFrequency: 'monthly',
+      url: url("/"),
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: url('/docs'),
-      changeFrequency: 'monthly',
+      url: url("/zh"),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: url("/docs"),
+      changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: url('/blog'),
-      changeFrequency: 'monthly',
+      url: url("zh/docs"),
+      changeFrequency: "weekly",
       priority: 0.8,
     },
-    ...languages.flatMap(lang => {
-      return source.getPages(lang).map(page => {
+    {
+      url: url("/blog"),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: url("zh/blog"),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    ...languages.flatMap((lang) => {
+      return source.getPages(lang).map((page) => {
         const lastModified = page.data.lastModified;
         return {
           url: url(page.url),
           lastModified: lastModified ? new Date(lastModified) : undefined,
-          changeFrequency: 'weekly',
+          changeFrequency: "weekly",
           priority: 0.5,
         } as MetadataRoute.Sitemap[number];
       });
     }),
-    ...languages.flatMap(lang => {
-      return blog.getPages(lang).map(page => {
+    ...languages.flatMap((lang) => {
+      return blog.getPages(lang).map((page) => {
         const lastModified = page.data.lastModified;
         return {
           url: url(page.url),
           lastModified: lastModified ? new Date(lastModified) : undefined,
-          changeFrequency: 'weekly',
+          changeFrequency: "weekly",
           priority: 0.5,
         } as MetadataRoute.Sitemap[number];
       });
