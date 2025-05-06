f66e99fd36460d4809e2a13f8fb162141be82032
Nooc <nooc@nooc.me>
2025-05-06 10:18:04 +0800

    Update the App Store link to campaign link

--- constants.ts
+++ constants.ts
@@ -1,1 +1,2 @@
-export const APP_STORE_LINK = "https://apps.apple.com/app/id6743597198";
+export const APP_STORE_LINK =
+  "https://apps.apple.com/app/apple-store/id6743597198?pt=126931111&ct=Official%20Website&mt=8";
