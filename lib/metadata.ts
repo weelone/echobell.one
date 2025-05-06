76ecc7e3da4671e56e068e581574bb44df19dbb5
Nooc <nooc@nooc.me>
2025-05-06 16:35:51 +0800

    Add itunes link into header

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,28 +1,31 @@
-import type { Metadata } from 'next/types';
+import type { Metadata } from "next/types";
 
 export function createMetadata(override: Metadata): Metadata {
   return {
+    itunes: {
+      appId: "id6743597198",
+    },
     ...override,
     openGraph: {
       title: override.title ?? undefined,
       description: override.description ?? undefined,
-      url: 'https://echobell.one',
-      images: '/images/banner.png',
-      siteName: 'Echobell',
+      url: "https://echobell.one",
+      images: "/images/banner.png",
+      siteName: "Echobell",
       ...override.openGraph,
     },
     twitter: {
-      card: 'summary_large_image',
-      creator: '@EchobellApp',
+      card: "summary_large_image",
+      creator: "@EchobellApp",
       title: override.title ?? undefined,
       description: override.description ?? undefined,
-      images: '/images/banner.png',
+      images: "/images/banner.png",
       ...override.twitter,
     },
   };
 }
 
 export const baseUrl =
-  process.env.NODE_ENV === 'development'
-    ? new URL('http://localhost:3000')
+  process.env.NODE_ENV === "development"
+    ? new URL("http://localhost:3000")
     : new URL(`https://echobell.one`);
